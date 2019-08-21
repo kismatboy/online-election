7ff9b7b4199571f84197c60aa502ddd055da1443
romeo sunil <justtotest@gmail.com>
2019-08-21 15:36:17 +0545

    email function added with phpmailer lib.

--- Home.php
+++ Home.php
@@ -168,23 +168,23 @@
                 <div class="row">
                     <div class="col-lg-8">
                         <div class="boxed-grey">
-                            <form id="contact-form">
+                            <form id="contact-form" method="POST" action="">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="name">Name</label>
-                                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required"/>
+                                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" name="name" />
                                         </div>
                                         <div class="form-group">
                                             <label for="email">Email Address</label>
                                             <div class="input-group">
                                                 <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-                                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" />
+                                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" name="email" />
                                             </div>
                                         </div>
                                         <div class="form-group">
                                             <label for="subject">Subject</label>
-                                            <select id="subject" name="subject" class="form-control" required="required">
+                                            <select id="subject" name="subject" class="form-control" required="required" name="subject">
                                                 <option value="na" selected="">Choose One:</option>
                                                 <option value="service">General Customer Service</option>
                                                 <option value="suggestions">Suggestions</option>
@@ -195,16 +195,28 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="name">Message</label>
-                                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required" placeholder="Message"></textarea>
+                                            <textarea name="msg" id="message" class="form-control" rows="9" cols="25" required="required" placeholder="Message"></textarea>
                                         </div>
                                     </div>
                                     <div class="col-md-12">
-                                        <button type="submit" class="btn btn-skin pull-right" id="btnContactUs">Send Message</button>
+                                        <input type="submit" name="submit" class="btn btn-skin pull-right" id="btnContactUs" value="Send Message">
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
+                    <?php 
+                    if(isset($_POST['submit'])){
+                        require_once 'PHPMailer/sendmail.php';
+                        echo '<script>alert("hello");</script>';
+                        $name=$_POST['name'];
+                        $email=$_POST['email'];
+                        $subject=$_POST['subject'];
+                        $msg=$_POST['msg'];
+
+                        send_mail('sunilsapkota9@gmail.com',$email,$name,$subject,$msg);
+                    }
+                    ?>
                     <div class="col-lg-4">
                         <div class="widget-contact">
                             <address>

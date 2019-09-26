4218228b4429f1553a289bf1c8899795eb5be9b6
romeo sunil <justtotest@gmail.com>
2019-09-26 19:40:05 +0545

    database update

--- databasestuff/election.sql
+++ databasestuff/election.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2019 at 07:22 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.1.30
+-- Generation Time: Sep 26, 2019 at 03:54 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -85,10 +83,10 @@ CREATE TABLE `election` (
 
 INSERT INTO `election` (`electionid`, `electiontitle`, `status`, `startdate`, `enddate`) VALUES
 (1, 'pradesh', 'end', '2019-08-13', '0000-00-00'),
-(2, 'college', '', '2019-08-07', '2019-08-12'),
+(2, 'college', 'end', '2019-08-07', '2019-08-22'),
 (3, 'college1', 'end', '2019-08-07', '2019-08-19'),
 (4, 'Chief Minister', 'end', '2019-08-14', '2019-08-19'),
-(5, 'Pradesh2', '', '2019-08-19', '0000-00-00');
+(5, 'Pradesh2', 'end', '2019-08-19', '2019-08-22');
 
 -- --------------------------------------------------------
 
@@ -97,12 +95,28 @@ INSERT INTO `election` (`electionid`, `electiontitle`, `status`, `startdate`, `e
 --
 
 CREATE TABLE `vote` (
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `voterid` int(11) NOT NULL,
   `candidateid` int(11) NOT NULL,
   `electionid` int(11) NOT NULL,
-  `count` int(11) NOT NULL
+  `count` int(11) NOT NULL,
+  `election_name` varchar(65) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vote`
+--
+
+INSERT INTO `vote` (`time`, `voterid`, `candidateid`, `electionid`, `count`, `election_name`) VALUES
+('2019-08-21 13:00:50', 1, 1, 1, 0, 'pradesh'),
+('2019-08-21 13:46:48', 1, 2, 1, 0, 'pradesh'),
+('2019-09-21 13:53:26', 1, 7, 5, 0, 'Pradesh2'),
+('2019-09-21 13:53:50', 1, 4, 5, 0, 'Pradesh2'),
+('2019-09-21 13:59:20', 1, 3, 1, 0, 'pradesh'),
+('2019-09-21 14:00:33', 1, 6, 5, 0, 'Pradesh2'),
+('2019-09-21 14:12:59', 2, 3, 1, 0, 'pradesh'),
+('2019-09-21 14:13:33', 2, 6, 5, 0, 'Pradesh2'),
+('2019-09-21 14:14:07', 2, 8, 3, 0, 'college1');
 
 -- --------------------------------------------------------
 
@@ -128,7 +142,7 @@ CREATE TABLE `voter` (
 --
 
 INSERT INTO `voter` (`voterid`, `votername`, `password`, `permanentadd`, `phone`, `citizenshipno`, `dob`, `gender`, `image`, `election_id`) VALUES
-(1, 'Ram', 'ram1', 'hemja', '23233', '23223', '1997-08-14', 0, '', 0),
+(1, 'sunil sapkota', '12345', 'lampata', '123456', '23223', '1997-08-14', 0, '', 0),
 (2, 'Sita', 'sita1', 'Chorepatan', '516413', '11111', '1987-08-14', 0, '', 0),
 (4, 'Shyam', 'shyam1', 'Chauthe', '65431654', '5565165646', '2000-08-05', 0, '', 0),
 (5, 'mayu', 'mayu1', 'Chorepatan', '1546465613', '154215', '1998-08-09', 0, '', 0),
@@ -171,20 +185,16 @@ ALTER TABLE `voter`
 --
 ALTER TABLE `candidate`
   MODIFY `candidateid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
 --
 -- AUTO_INCREMENT for table `election`
 --
 ALTER TABLE `election`
   MODIFY `electionid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
 --
 -- AUTO_INCREMENT for table `voter`
 --
 ALTER TABLE `voter`
   MODIFY `voterid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
